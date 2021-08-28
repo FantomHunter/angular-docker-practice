8725cde9d81e9a8f4c0dced9f03bb72f13119ef6
AAVN\hvhai <m.hovanhai@gmail.com>
2021-08-28 21:47:25 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  component: HomeComponent,
+  path: ''
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,38 @@
+import { GreetingResponse } from './../../core/models/greetting-response.model';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  userList: number[] = [1, 2, 3, 4, 5, 6];
+  selectedUser = 1;
+  backendMessage$: Observable<string> = of('');
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {}
+
+  loadData() {
+    this.backendMessage$ = this.httpClient
+      .get<GreetingResponse>(
+        environment.backendUrl + 'greeting-from-db?id=' + this.selectedUser
+      )
+      .pipe(
+        map((res: GreetingResponse) => {
+          return res.message;
+        }),
+        catchError(async (ery) => 'Not found')
+      );
+  }
+
+  onUserBoxChanged(user: number) {
+    this.selectedUser = user;
+  }
+}
